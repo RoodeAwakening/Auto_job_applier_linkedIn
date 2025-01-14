e04bfbbfb90186c36381b88eb742abc711a48672
Derek Roode <derek.i.roode@gmail.com>
2025-01-14 12:23:23 -0500

    error

--- runAiBot.py
+++ runAiBot.py
@@ -7,11 +7,6 @@ Copyright (C) 2024 Sai Vignesh Golla
 License:    GNU Affero General Public License
             https://www.gnu.org/licenses/agpl-3.0.en.html
             
-            
-GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
-
-version:    24.12.3.10.30
-'''
 
 GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
 
